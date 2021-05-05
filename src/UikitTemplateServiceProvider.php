d9907dd3fc758767ef91b6044250509476c47a88
Vadym <vadymydav@gmail.com>
2021-05-05 14:53:33 +0200

    Lavary menu

--- src/UikitTemplateServiceProvider.php
+++ src/UikitTemplateServiceProvider.php
@@ -3,6 +3,7 @@
 namespace IlBronza\UikitTemplate;
 
 use Illuminate\Support\ServiceProvider;
+use Lavary\Menu\Menu;
 
 class UikitTemplateServiceProvider extends ServiceProvider
 {
@@ -41,6 +42,17 @@ class UikitTemplateServiceProvider extends ServiceProvider
         $this->app->singleton('uikittemplate', function ($app) {
             return new UikitTemplate;
         });
+
+        // $this->app->singleton(Menu::class, function ($app) {
+        //     return new Menu([config('laravel-menu.settings'), config('laravel-menu.views')] );
+        // });
+
+        // $this->app->make('MyNavBar', function ($menu) {
+        //     $menu->add('Home');
+        //     $menu->add('About', 'about');
+        //     $menu->add('Services', 'services');
+        //     $menu->add('Contact', 'contact');
+        // });
     }
 
     /**
